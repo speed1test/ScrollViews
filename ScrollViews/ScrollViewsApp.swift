a90df836ce140a45d35c56a6391c190ba3a5df07
Edwin <edwin12sv@gmail.com>
2023-12-28 23:47:51 -0600

    Initial Commit

---
+++ ScrollViews/ScrollViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewsApp.swift
+//  ScrollViews
+//
+//  Created by elsalvador on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
