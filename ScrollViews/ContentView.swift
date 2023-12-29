0c9a95661367bba03fe85ddcafab2c135bc0eae2
Edwin <edwin12sv@gmail.com>
2023-12-29 00:12:37 -0600

    Mejoras y se agrega boton para ocultar vistas

--- ScrollViews/ContentView.swift
+++ ScrollViews/ContentView.swift
@@ -8,35 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isScrollViewVisible = true
+
     var body: some View {
-        HStack {
-            // Parte izquierda de la pantalla
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Contenido fuera del ScrollView")
-                    .padding()
-                    .multilineTextAlignment(.center)
+        NavigationView {
+            VStack {
+                HStack {
+                    // Parte izquierda de la pantalla
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Contenido fuera del ScrollView")
+                            .padding()
 
-                // ScrollView que cubre solo una parte de la pantalla
-                MyScrollView()
-                    .frame(width: 200)
-                    .padding()
+                        // ScrollView que cubre solo una parte de la pantalla
+                        if isScrollViewVisible {
+                            MyScrollView()
+                                .frame(width: 200, height: 400) // Ajusta la altura según tus necesidades
+                                .padding()
+                        }
 
-                // Spacer para centrar los elementos verticalmente
-                Spacer()
+                        // Spacer para centrar los elementos verticalmente
+                        Spacer()
+                    }
+
+                    // Parte derecha de la pantalla
+                    SecondView()
+                        .padding()
+                }
             }
-
-            // Parte derecha de la pantalla
-            SecondView()
-                .padding()
+            .navigationTitle("Scroll Views Demo")
+            .toolbar {
+                // Botón en la barra de herramientas
+                ToolbarItem(placement: .bottomBar) {
+                    Button(action: {
+                        isScrollViewVisible.toggle()
+                    }) {
+                        Text(isScrollViewVisible ? "Ocultar ScrollView" : "Mostrar ScrollView")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                }
+            }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
+
 
 struct MyScrollView: View {
     var body: some View {
         ScrollView {
             VStack {
-                ForEach(1...20, id: \.self) { index in
+                ForEach(1...50, id: \.self) { index in
                     Text("Elemento \(index)")
                         .padding()
                 }
@@ -53,7 +77,6 @@ struct SecondView: View {
             Text("Segunda vista con una CircleImage")
                 .font(.headline)
                 .padding()
-                .multilineTextAlignment(.center)
 
             CircleImage(imageURL: "https://docs-assets.developer.apple.com/published/2b2fe5cb06e9b4e3b9bb6496ed407797/010607.png")
                 .frame(width: 100, height: 100)
@@ -91,7 +114,7 @@ struct CircleImage: View {
     }
 }
 
-
 #Preview {
     ContentView()
 }
+
